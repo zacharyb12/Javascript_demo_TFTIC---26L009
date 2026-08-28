debc3281be6db628088c2a3ca25a691fb946a691
zacharyb12 <boualemzachary89@yahoo.fr>
2026-08-28 11:17:35 +0200

    correction exo 1

---
+++ 99.exercices-corrections/exo1/script.js
@@ -0,0 +1,22 @@
+
+// demander le nom et le stocker dans une variable
+
+let nom = prompt("Veuillez entrez votre nom :")
+console.log(nom);
+
+// afficher un message : bonjour + nom
+
+// alert("Bonjour " + nom)
+alert(`Bonjour ${nom}`)
+
+// alert('Bonjour l\'equipe')
+
+const age = prompt("Entrez votre age :")
+// console.log(age + 2)
+console.log(typeof(age))
+
+alert(`Tu t'appelles ${nom} et tu as ${age} ans.`)
+
+// ''
+// `` altgr + µ
+// ´´ altgr + ù
